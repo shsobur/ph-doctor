607f7249fcd747540c404f3ddcddd18c7468e2d0
shsobur <shsoburhossen951@gmail.com>
2025-04-21 19:58:25 +0600

    Navbar responsive design completed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./Navbar.css";
+import { FaBars } from "react-icons/fa";
 import { GiGloves } from "react-icons/gi";
 import { IoSearchOutline } from "react-icons/io5";
+import { RxCross1, RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
     <>
       <nav>
         <div className="main_navbar_container">
-
           <div className="navbar_logo_container">
             <h1>
               <span>
@@ -17,7 +21,19 @@ const Navbar = () => {
             </h1>
           </div>
 
-          <div className="navbar_routes_container">
+          <div id="desktop_routes" className="navbar_routes_container">
+            <ul>
+              <li>Home</li>
+              <li>My-Bookings</li>
+              <li>Blogs</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+
+          <div
+            id={!menu ? "active_mobile_routes" : ""}
+            className="navbar_mobile_routes_container"
+          >
             <ul>
               <li>Home</li>
               <li>My-Bookings</li>
@@ -33,6 +49,17 @@ const Navbar = () => {
             </span>
           </div>
 
+          <button id="menu_container" onClick={() => setMenu(!menu)}>
+            {menu ? (
+              <h3 id="menu_bar_off">
+                <RxCross1 />
+              </h3>
+            ) : (
+              <h3 id="menu_bar_on">
+                <FaBars />
+              </h3>
+            )}
+          </button>
         </div>
       </nav>
     </>
